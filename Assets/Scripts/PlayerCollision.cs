4886b8a439c672b616371309acb69984bc3ede24
jcerbito <cerbitojolessa3396@gmail.com>
2018-02-26 23:22:20 +0800

    + Additional Playe collision on obstacles

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerCollision : MonoBehaviour {
+
+    [SerializeField] ParticleSystem effect;
+	[SerializeField] float delayTime = 1f;
+
+	void Start(){
+		effect.Stop();
+	}
+
+	void Explode(){
+		effect.Play();
+	}
+
+	void OnCollisionEnter(Collision other){
+		Explode();
+		//Invoke("Restart", delayTime);
+	}
+
+	void Restart(){
+		SceneManager.LoadScene(1);
+	}
+}
