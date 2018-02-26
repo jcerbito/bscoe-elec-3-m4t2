2d4da7f0a182781e001446c98206773222dc1222
jcerbito <cerbitojolessa3396@gmail.com>
2018-02-27 04:58:10 +0800

    + Additional Destroy the enemy on bullet hit

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnemyCollision : MonoBehaviour {
+
+    [SerializeField] ParticleSystem effect;
+	[SerializeField] GameObject bull;
+	[SerializeField] float delayTime = 1f;
+
+	void Explode(){
+		effect.Play();
+		Destroy(gameObject, delayTime);
+	}
+
+	void OnTriggerEnter(Collider bull){
+		Explode();
+		
+	}
+
+
+}
